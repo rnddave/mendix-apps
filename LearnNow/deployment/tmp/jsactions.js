cff1448a4a5c6095e8f561baf11b3648c4320f0f
David Dickinson <36966460+rnddave@users.noreply.github.com>
2023-03-13 21:55:40 +0000

    had to rebuild again due to unknown error (that was the error message...)

---
+++ LearnNow/deployment/tmp/jsactions.js
@@ -0,0 +1,2 @@
+export const NanoflowCommons$Share = async () => (await import("E:/SHARED/GitHub/mendix-apps/LearnNow/javascriptsource/nanoflowcommons/actions/Share")).Share;
+export const NanoflowCommons$OpenMap = async () => (await import("E:/SHARED/GitHub/mendix-apps/LearnNow/javascriptsource/nanoflowcommons/actions/OpenMap")).OpenMap;
